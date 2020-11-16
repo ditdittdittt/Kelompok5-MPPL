e5e26371c8dd246eb3505da2aa3ab8d53bfbbca9
Fakhor <47167443+Fakhor@users.noreply.github.com>
2020-11-16 20:01:38 +0700

    nambahin ui login

--- resources/views/auth/login.blade copy.php
+++ resources/views/auth/login.blade copy.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- <title>Login Template</title> -->
+    <title>Login Template</title>
     <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
@@ -18,7 +18,6 @@
             <div class="row">
                 <div class="col-sm-6 login-section-wrapper">
                     <div class="card col d-flex justify-content-center">
-                        <!-- <div class="card-header">{{ __('Login') }}</div> -->
                         <div class="login-wrapper my-auto mx-auto">
                             <h5 class="d-flex justify-content-center">WELCOME TO</h5>
                             <h1 class="d-flex justify-content-center login-title">YOUR IPB MAPS</h1>
@@ -49,14 +48,11 @@
                                         <strong>{{ $message }}</strong>
                                     </span>
                                     @enderror
-
-                                    <!-- CHECK BOXES REMEMBER -->
-
                                 </div>
 
                                 <!-- CHECK BOXES REMEMBER -->
-                                <div class="form-group mb-4">
-                                    <div>
+                                <div class="form-group row">
+                                    <div class="col-md-6 offset-md-4">
                                         <div class="form-check">
                                             <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -68,27 +64,21 @@
                                 </div>
 
                                 <!-- <input name="login" id="login" class="btn btn-block login-btn" type="button" value="LOGIN"> -->
-                                <div>
-                                    <div>
-                                        <button type="submit" class="btn btn-block login-btn">
-                                            {{ __('LOGIN') }}
+
+                                <div class="form-group row mb-0">
+                                    <div class="col-md-8 offset-md-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ __('Login') }}
                                         </button>
 
                                         @if (Route::has('password.request'))
-                                        <button class="btn btn-link" href="{{ route('password.request') }}">
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
                                             {{ __('Forgot Your Password?') }}
-                                        </button>
+                                        </a>
                                         @endif
                                     </div>
                                 </div>
 
-                                <!-- LINE -->
-                                <hr />
-
-                                <!-- REGISTER -->
-                                <div>
-                                    <a class="btn btn-block login-btn" href="{{ route('register') }}">REGISTER</a>
-                                </div>
                             </form>
                             <!-- <a href="#!" class="forgot-password-link">Forgot password?</a>
               <p class="login-wrapper-footer-text">Don't have an account? <a href="#!" class="text-reset">Register here</a></p> -->
