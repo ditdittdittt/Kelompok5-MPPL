ec097615af4de19851cf1c03abf097d066f6015e
ditdittdittt <ditdittdittt@gmail.com>
2020-11-23 19:56:15 +0700

    [yudit] fix routing

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = "/space";
 
     /**
      * Create a new controller instance.
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'SpaceController@index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('space', 'SpaceController');
 Route::get('/browse/space', 'SpaceController@browse')->name('space.browse');
